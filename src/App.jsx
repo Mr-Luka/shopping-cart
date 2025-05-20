7752e40572d6cf6da8ebc0c31e83d4bdbb495596
Luka <luka.luka.luka93@gmail.com>
2025-05-20 12:48:42 -0700

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
-
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import Product from './components/Product.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 import CartContextProvider from './store/shoping-cart-context.jsx';
 
-import {CartContext} from './store/shoping-cart-context.jsx';
 
 function App() {
   
